050fb99d98a069b2f8bd093bbd38de7671dbd111
DJ Hepburn <dj@djhepburn.com>
2022-03-25 03:17:06 -0400

    Abstract bool type to its own header

---
+++ src/include/bool.h
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_BOOL_H
+#define INCLUDE_BOOL_H
+
+typedef int boolean;
+#define bool boolean
+
+#define false 0
+#define true  1
+
+#endif /* INCLUDE_BOOL_H */
--- src/include/shared.h
+++ src/include/shared.h
@@ -66,14 +66,7 @@ typedef vec_t vec3_t[3];
 typedef vec_t vec4_t[4];
 typedef vec_t vec5_t[5];
 
-// instead of #include <stdbool.h>
-// sizeof(boolean) needs to be 4 otherwise structures don't match up
-typedef int boolean;
-#define bool boolean
-
-#define false	0
-#define true	1
-// typedefs
+#include "bool.h"
 
 // from ioquake
 #define PAD(base, alignment)	(((base)+(alignment)-1) & ~((alignment)-1))
@@ -581,30 +574,5 @@ boolean info_validate(const char *s);
 void info_set_value_for_key(char *s, const char *key, const char *value);
 void info_set_value_for_key_big(char *s, const char *key, const char *value);
 void info_print(const char *s);
-
-//
-//
-// parse.c
-//
-//
-void com_begin_parse_session(const char *name);
-void com_end_parse_session(void);
-void com_reset_parse_session(void);
-void com_set_field_0x40c(bool val);
-int com_get_current_parse_line(void);
-void com_script_error(const char *msg, ...);
-void com_script_warning(const char *msg, ...);
-void com_unget_token(void);
-char *com_parse(char **buf_p);
-char *com_parse_on_line(char **buf_p);
-void com_match_token(char **buf_p, char *match, bool warning);
-void com_skip_braced_section(char **program);
-void com_skip_rest_of_line(char **data_p);
-char *com_parse_rest_of_line(char **data_p);
-float com_parse_float(char **buf_p);
-int com_parse_int(char **buf_p);
-void com_parse_1d_matrix(char **buf_p, int x, float *m);
-void com_parse_2d_matrix(char **buf_p, int y, int x, float *m);
-void com_parse_3d_matrix(char **buf_p, int z, int y, int x, float *m);
 
 #endif // __SHARED_H__
