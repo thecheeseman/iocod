4289a3d7970362e04307632deb289c3815995e9d
DJ Hepburn <dj@djhepburn.com>
2022-05-15 01:14:39 -0400

    docs: add GPL header to log.c

--- src/core/log.c
+++ src/core/log.c
@@ -1,3 +1,33 @@
+/*
+================================================================================
+iocod
+Copyright (C) 2021-2022 thecheeseman
+
+This file is part of the iocod GPL source code.
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+================================================================================
+*/
+
+/**
+ * @file   log.c
+ * @author thecheeseman
+ * @date   2022-05-14
+ *
+ * Logging functionality.
+ */
+
 #include <errno.h>
 #include <stdio.h>
 #include <time.h>
