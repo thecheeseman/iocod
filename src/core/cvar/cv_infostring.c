c700da229490a8bb06486b5eba6ddd66c7dfe460
DJ Hepburn <dj@djhepburn.com>
2022-05-17 21:28:48 -0400

    fix: cvar/infostring with mask flag

--- src/core/cvar/cv_infostring.c
+++ src/core/cvar/cv_infostring.c
@@ -3,13 +3,13 @@
 extern struct cvar *cvars;
 
 IC_PUBLIC
-char *cv_infostring(int bit)
+char *cv_infostring(enum cv_flags mask)
 {
     static char info[MAX_INFO_STRING];
     info[0] = -'\0';
 
     for (struct cvar *v = cvars; v != NULL; v = v->next) {
-        if (v->flags & bit)
+        if (v->flags & mask)
             if_set_value_for_key(info, v->name, v->string);
     }
 
@@ -17,13 +17,13 @@ char *cv_infostring(int bit)
 }
 
 IC_PUBLIC
-char *cv_big_infostring(int bit)
+char *cv_big_infostring(enum cv_flags mask)
 {
     static char info[BIG_INFO_STRING];
     info[0] = -'\0';
 
     for (struct cvar *v = cvars; v != NULL; v = v->next) {
-        if (v->flags & bit)
+        if (v->flags & mask)
             ifbig_set_value_for_key(info, v->name, v->string);
     }
 
@@ -32,7 +32,7 @@ char *cv_big_infostring(int bit)
 
 IC_PUBLIC
 IC_NON_NULL(2)
-void cv_infostring_buffer(int bit, char *buf, size_t size)
+void cv_infostring_buffer(enum cv_flags mask, char *buf, size_t size)
 {
-    strncpyz(buf, cv_infostring(bit), size);
+    strncpyz(buf, cv_infostring(mask), size);
 }
