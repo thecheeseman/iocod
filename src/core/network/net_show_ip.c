2bb57f544c10e9171ee07c1979da6f5b64b918a6
DJ Hepburn <dj@djhepburn.com>
2022-06-18 23:43:20 -0400

    fix: wrong parameter order for net_sockaddr_to_string

--- src/core/network/net_show_ip.c
+++ src/core/network/net_show_ip.c
@@ -4,9 +4,9 @@ IC_PUBLIC
 void net_show_ip(void)
 {
     for (int i = 0; i < numip; i++) {
-        char s[MAX_ADDR_STRING];
-        net_sockaddr_to_string(s, sizeof(s), 
-                               (struct sockaddr *) &localip[i].addr);
+        char s[MAX_ADDR_STRING] = { 0 };
+        net_sockaddr_to_string((struct sockaddr *) &localip[i].addr, 
+                               sizeof(s), s);
 
         if (localip[i].type == NA_IP)
             log_print("IPv4: %s\n", s);
