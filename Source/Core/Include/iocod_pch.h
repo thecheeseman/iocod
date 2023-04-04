4da59523a4d57eddc9d16f694275b6089f372c50
thecheeseman <1952103+thecheeseman@users.noreply.github.com>
2023-04-03 21:26:11 -0400

    chore: remove old premake config and pch header

--- Source/Core/Include/iocod_pch.h
+++
@@ -1,8 +0,0 @@
-// SPDX-FileCopyrightText: 2023 thecheeseman
-//
-// SPDX-License-Identifier: GPL-3.0-or-later
-
-#include <Core/Assert.h>
-#include <Core/Platform.h>
-#include <Core/Stringify.h>
-#include <Core/Version.h>
