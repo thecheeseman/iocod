ac97b6ba52e59de42592f3606308e55e693d89b6
thecheeseman <1952103+thecheeseman@users.noreply.github.com>
2023-05-15 20:17:26 -0400

    chore: update NarrowCast

--- Source/Core/Tests/test-narrow_cast/test-narrow_cast.cpp
+++ Source/Core/Tests/test-narrow_cast/test-narrow_cast.cpp
@@ -5,7 +5,7 @@
 #include <complex>
 #include <limits>
 
-#include <Core/narrow_cast.hpp>
+#include <Core/NarrowCast.hpp>
 #include <catch2/catch_test_macros.hpp>
 
 TEST_CASE("narrow_cast", "[narrow_cast]")
