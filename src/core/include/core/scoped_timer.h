95c322e987e186dafe4e40e1efdedb55f0eb3596
thecheeseman <1952103+thecheeseman@users.noreply.github.com>
2023-05-21 00:47:55 -0400

    feat: add scoped timer debug utility
    Signed-off-by: thecheeseman <1952103+thecheeseman@users.noreply.github.com>

---
+++ src/core/include/core/scoped_timer.h
@@ -0,0 +1,80 @@
+// SPDX-FileCopyrightText: 2023 thecheeseman
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#ifndef CORE_SCOPED_TIMER_H
+#define CORE_SCOPED_TIMER_H
+
+#include <core/class_non_copyable.h>
+#include <core/types.h>
+
+#include <chrono>
+#include <map>
+#include <string_view>
+
+namespace iocod {
+
+class ScopedTimer final {
+public:
+    CLASS_NON_COPYABLE(ScopedTimer)
+
+    explicit ScopedTimer(const char* function_, const char* filename_, u32 line)
+        : function(function_),
+          filename(filename_),
+          line(line),
+          start(Clock::now())
+    {
+        if (!initialized) {
+            std::atexit(DumpToCSV);
+            initialized = true;
+        }
+    }
+
+    ~ScopedTimer()
+    {
+        auto end = Clock::now();
+        auto time = std::chrono::duration_cast<Nanoseconds>(end - start);
+
+        auto& data = timers[function];
+        data.function = function;
+        data.filename = filename;
+        data.line = line;
+        data.total_time += time;
+        data.function_calls++;
+        data.min_time = std::min(data.min_time, time);
+        data.max_time = std::max(data.max_time, time);
+    }
+
+    static void DumpToCSV();
+    static void DumpToStdout();
+
+private:
+    using Clock = std::chrono::high_resolution_clock;
+    using Time = std::chrono::time_point<Clock>;
+    using Nanoseconds = std::chrono::nanoseconds;
+
+    Time start;
+    const char* filename;
+    const char* function;
+    u32 line;
+
+    struct TimerData {
+        const char* filename;
+        const char* function;
+        u32 line = 0;
+        u32 function_calls = 0;
+
+        Nanoseconds total_time = Nanoseconds(0);
+        Nanoseconds min_time = Nanoseconds::max();
+        Nanoseconds max_time = Nanoseconds::min();
+    };
+
+    static inline std::map<std::string_view, TimerData> timers{};
+    static inline bool initialized = false;
+};
+
+#define CreateScopedTimer() ScopedTimer timer(__FUNCTION__, __FILE__, __LINE__)
+
+} // namespace iocod
+
+#endif // CORE_SCOPED_TIMER_H
---
+++ src/core/src/scoped_timer.cpp
@@ -0,0 +1,69 @@
+// SPDX-FileCopyrightText: 2023 thecheeseman
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+#include <core/scoped_timer.h>
+
+#include <cstring>
+#include <fstream>
+#include <iostream>
+
+namespace iocod {
+
+using Nanoseconds = std::chrono::nanoseconds;
+using Seconds = std::chrono::seconds;
+
+namespace {
+
+std::string ConvertTime(Nanoseconds time)
+{
+    char fmt[64]{};
+
+    if (time < Nanoseconds(1000))
+        snprintf(fmt, sizeof(fmt), "%lld ns", time.count());
+    else if (time < Nanoseconds(1000000))
+        snprintf(fmt, sizeof(fmt), "%.2g us", static_cast<double>(time.count()) / 1000.0);
+    else if (time < Nanoseconds(1000000000))
+        snprintf(fmt, sizeof(fmt), "%.2g ms", static_cast<double>(time.count()) / 1000000.0);
+    else
+        snprintf(fmt, sizeof(fmt), "%.2g s", static_cast<double>(time.count()) / 1000000000.0);
+
+    return fmt;
+}
+
+}
+
+void ScopedTimer::DumpToCSV()
+{
+    static std::size_t counter = 0;
+    const std::string csvname = "scopedtimer" + std::to_string(counter++) + ".csv";
+    std::ofstream file(csvname);
+
+    file << "function,calls,total time,min time,max time,avg time,cps,file\n";
+
+    for (const auto& [name, data] : timers) {
+        file << data.function << "," << data.function_calls << "," << ConvertTime(data.total_time) << ","
+             << ConvertTime(data.min_time) << "," << ConvertTime(data.max_time) << ","
+             << ConvertTime(data.total_time / data.function_calls) << ","
+             << (Seconds(1) / (data.total_time / data.function_calls)) << "," << data.filename << ":"
+             << data.line << "\n";
+    }
+
+    file.close();
+}
+
+void ScopedTimer::DumpToStdout()
+{
+    std::cerr << "ScopedTimer data:\n";
+
+    for (const auto& [name, data] : timers) {
+        std::cerr << "    " << data.filename << ":" << data.line << " " << data.function << "\n";
+        std::cerr << "        calls:      " << data.function_calls << "\n";
+        std::cerr << "        total time: " << data.total_time << "\n";
+        std::cerr << "        min time:   " << data.min_time << "\n";
+        std::cerr << "        max time:   " << data.max_time << "\n";
+        std::cerr << "        avg time:   " << data.total_time / data.function_calls << "\n";
+    }
+}
+
+} // namespace iocod
